7e846d15f4d8ba710474192da9a2217f485ff7ad
Jam <guo3jin3cai@gmail.com>
2017-08-17 08:49:34 +0800

    add

--- p3_chat_vue_socket/client/src/components/index.vue
+++ p3_chat_vue_socket/client/src/components/index.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="c-talk">
-
+    <h4>index.vue</h4>
   </div>
 </template>
 
 <script>
-  import './../sass/index.scss'
-
-  export default {
-    data () {
-      return {
-        data: ''
-      }
+//  import './../sass/index.scss'
+export default {
+  data () {
+    return {
+      data: ''
     }
   }
+}
 </script>
 
 <style lang="scss" scope>
-
+h4{
+  margin: 0;
+  font-size: 25px;
+}
 </style>
