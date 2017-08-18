6dd891f6a33269bb0dd00f0599a490421f7edf3f
Gjincai <1262868349@qq.com>
2017-08-18 18:32:42 +0800

    html

--- p3_chat_vue_socket/client/src/components/index.vue
+++ p3_chat_vue_socket/client/src/components/index.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="c-talk">
-    <h4>index.vue</h4>
+    <h4>index</h4>
+    <div class="talk-cont">
+      <ul class="cont-ul">
+        <li class="mes-li mes-li-left">
+          <div class="li-head"><img src="#"/></div>
+          <div class="li-box">
+            <p class="box-name">用户a</p>
+            <p class="box-mes">哈哈哈哈哈哈哈哈</p>
+          </div>
+        </li>
+        <li class="mes-li mes-li-left">
+          <div class="li-head"><img src="#"/></div>
+          <div class="li-box">
+            <p class="box-name">用户a</p>
+            <p class="box-mes">哈哈哈哈哈哈哈哈</p>
+          </div>
+        </li>
+        <li class="mes-li mes-li-right">
+          <div class="li-head"><img src="#"/></div>
+          <div class="li-box">
+            <p class="box-name">用户a</p>
+            <p class="box-mes">哈哈哈哈哈哈哈哈</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="talk-edit"></div>
   </div>
 </template>
 
